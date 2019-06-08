6c1d8f5d8f723fda58d932f4dcf71f1475b1e3eb
student21 <student21@pcoip.ki.agh.edu.pl>
2019-06-08 14:05:17 +0200

    Initial commit

---
+++ src/main/java/calculator/Calculator.java
@@ -0,0 +1,20 @@
+package calculator;
+
+public class Calculator {
+
+	public static void main(String[] args) {
+        System.out.println("Hello World!"); // Display the string.
+    }
+
+	public int add(int i, int j) {
+		return i+j;
+	}
+
+	public int subtract(int i, int j) {
+		return i-j;
+	}
+
+	public int multiply(int i, int j) {
+		return i*j;
+	}
+}
---
+++ src/test/java/calculator/CalculatorTest.java
@@ -0,0 +1,31 @@
+package calculator;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class CalculatorTest {
+
+	Calculator calc;
+	
+	@Before
+	public void setup() {
+		calc = new Calculator();
+	}
+	
+	@Test
+	public void testAdd() {
+		assertEquals(5, calc.add(2,3));
+	}
+	
+	@Test
+	public void testSubtract() {
+		assertEquals(12, calc.subtract(15,3));
+	}
+	
+	@Test
+	public void testMultiply() {
+		assertEquals(6, calc.multiply(2,3));
+	}
+
+}
